7b189a98cf18291b8a5c2e1f0d19ea4018a5aabe
madhulikaayare <madhulikaayare@gmail.com>
2023-02-16 08:21:44 +0530

    recent value
    recent value

--- helloWorld.js
+++ helloWorld.js
@@ -8,3 +8,9 @@ var city;
 console.log(city); //variable declaration
 city = "Pune"; //variable initialization
 console.log(city);
+var age = "22"//variable declaration and initialization
+age = "23";
+age = "24";
+console.log(age);
+var university = "sppu"; //initialize
+console.log(university);
