93dd0912a970d2ce8964204878515a8ffd1dc729
madhulikaayare <madhulikaayare@gmail.com>
2023-02-16 07:58:07 +0530

    variable declare and initialize
    variable declare and initialize

--- helloWorld.js
+++ helloWorld.js
@@ -1,3 +1,10 @@
 // Personal Details
-console.log(" Mohit sharma ");
-console.log(" 31 ");
+console.log(" Madhulika Ayare ");
+console.log(" 22 ");
+console.log(" pune ");
+var fullName = "Madhulika Ayare";
+console.log(fullName);
+var city;
+console.log(city); //variable declaration
+city = "Pune"; //variable initialization
+console.log(city);
