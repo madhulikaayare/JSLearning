c46839debbc3d8293dfda982fd42e9cc1d0b4ebd
madhulikaayare <madhulikaayare@gmail.com>
2023-02-16 09:06:23 +0530

    updated
    updated

--- 01_variable_assign.js
+++ 01_variable_assign.js
@@ -1,13 +1,18 @@
+console.log("****1****");
 console.log(myFullName);
-var myFullName = "Madhulika Milind Ayare";
+var myFullName = "Madhulika Ayare";
 console.log(myFullName);
+myFullName = "Madhulika Milind Ayare";
+console.log(myFullName);
+console.log("****2****");
 var myLove = "I love only JS";
 console.log(myLove);
+console.log("****3****");
 var firstName = "Madhulika";
 var lastName = "Ayare";
 var age = "22";
-var address = "Pune";
+var address = "pune";
 var pincode = "411038";
-var gender = "Female";
-var isMarried = "no";
-console.log(  firstName,  lastName, age,  address, pincode,  gender, isMarried);
+var gender = "Female"
+var isMarried = "No";
+console.log(firstName, lastName, age, address, pincode, gender, isMarried);
