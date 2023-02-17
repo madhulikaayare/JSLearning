ca590ca86803e1fa24e78f2758247c49a5c0931c
madhulikaayare <madhulikaayare@gmail.com>
2023-02-17 08:39:12 +0530

    updated

---
+++ 02_datatypes.js
@@ -0,0 +1,25 @@
+var myName = 'Virat Kohli';
+var fatherName = "Yogesh Kohli"
+
+var age = "31"; //string
+var weight = 82.5; //num
+
+var isMarried = true;
+var isLazy = false; 
+
+var typeOfMyName  = typeof myName;
+console.log("Typeof variable myName: ",  typeOfMyName);
+console.log("typeOf variable fatherName:", typeof fatherName);
+console.log("typeof variable age:", typeof age);
+console.log("typeof variable weight", typeof weight);
+console.log("typeof variabke isMarried:", typeof isMarried);
+console.log("typeof variabke isLazy:", typeof isLazy);
+
+var graduation
+console.log("value of graduation variable is:", graduation); 
+console.log("typeof variabke graduation:", typeof graduation);
+ var city = null
+ console.log("value of city variable is:", city); 
+console.log("typeof variabke city:", typeof city);
+age = true;
+console.log(typeof age);
