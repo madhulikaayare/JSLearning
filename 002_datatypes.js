6e80f027936928ca70384ab61b3d037c080632de
madhulikaayare <madhulikaayare@gmail.com>
2023-02-21 06:57:21 +0530

    updated

--- 002_datatypes.js
+++ 002_datatypes.js
@@ -5,18 +5,12 @@ var age = "31"; //string
 var weight = 82.5; //num
 
 var isMarried = true;
-var isLazy = false; 
+var isLazy = false;  
 
 var typeOfMyName  = typeof myName;
 console.log("Typeof variable myName: ",  typeOfMyName);
 console.log("typeOf variable fatherName:", typeof fatherName);
-console.log("typeof variable age:", typeof age);
-console.log("typeof variable weight", typeof weight);
-console.log("typeof variabke isMarried:", typeof isMarried);
-console.log("typeof variabke isLazy:", typeof isLazy);
-
-var graduation
-console.log("value of graduation variable is:", graduation); 
+console.log("typeof variable age:", typeof age); 
 console.log("typeof variabke graduation:", typeof graduation);
  var city = null
  console.log("value of city variable is:", city); 
@@ -32,3 +26,10 @@ num2 = 200;
  var temp = num1;
  num1 = num2;
  num2 = temp;
+
+ var str1 = "sweety";
+ var str2 = "cute";
+ var temp = str1;
+ str1 = str2;
+ str2 = temp;
+ console.log(str1 , str2);
---
+++ 03_function.js
@@ -0,0 +1,60 @@
+// var num1 = "100";
+// var num2 = "200";
+// function swapVariables(value1,value2){
+// console.log( "before swap : "value1,value2);
+// var temp = value1;
+// value1 = value2;
+// value2 = temp;
+// console.log("after swap:" value1,value2);
+// }
+// swapVariables(num1,num2);
+
+
+//func with no arg and no return value
+function showFullName(){
+    console.log("My Full Name is : Madhulika Ayare");
+}
+showFullName();
+
+//func with  arg and no return value
+function showAge(age){
+console.log("My name is ", age);
+}
+showAge(22);
+
+//func with  no arg and  return value
+function fullName(){
+    var Name = "Madhulika Ayare";
+    return Name;
+}
+var fName = fullName();
+console.log(fName);
+
+//func with   arg and  return value
+function sumOfNumbers(num1,num2,num3){
+var sum = num1 + num2 + num3;
+return sum;
+}
+var sumResult = sumOfNumbers(10,45,79.56);
+console.log(sumResult);
+
+num1 = 568;
+num2 = 789;
+
+
+
+
+
+function swapVariables(value1, value2) {
+    console.log("Before Swap: ", value1, value2);
+    var temp = value1;
+    value1 = value2;
+    value2 = temp;
+    console.log("After Swap: ", value1, value2);
+    return "Swapping variables successfully completed";
+  }
+  var swapResult = swapVariables(num1, num2); // Function call or Function invoke
+  console.log(swapResult);
+  
+  var swapResult2 =  swapVariables(str1, str2);
+  console.log(swapResult2);
