e7fa41b5c8eebc24323f22cb2b86ca7c5edb63a8
madhulikaayare <madhulikaayare@gmail.com>
2023-02-21 07:41:51 +0530

    string position

--- 04_string.js
+++ 04_string.js
@@ -1,2 +1,10 @@
 var greet = "Good Morning";
 console.log(typeof greet);
+ 
+console.log("Total number of characters available into this string variable - greet");
+var greetlength = greet.length;
+console.log(greetlength);
+
+console.log("Find character by index value");
+var charAtIndex3 = greet.charAt(3);
+console.log("character available at index 3 is:",charAtIndex3);
