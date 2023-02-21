ad48420df65eeb3a5c0a95d173dd3838f9749996
madhulikaayare <madhulikaayare@gmail.com>
2023-02-21 07:44:34 +0530

    last character

--- 04_string.js
+++ 04_string.js
@@ -8,3 +8,7 @@ console.log(greetlength);
 console.log("Find character by index value");
 var charAtIndex3 = greet.charAt(3);
 console.log("character available at index 3 is:",charAtIndex3);
+
+console.log("Find the last character");
+var charAtLastIndex = greet.charAt(greetlength-1);
+console.log("last character is:", charAtLastIndex);
