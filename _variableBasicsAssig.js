b15114cdc24e690cd48fa2d06e3f22fc8c533593
madhulikaayare <madhulikaayare@gmail.com>
2023-02-22 17:19:12 +0530

    assingment

---
+++ _variableBasicsAssig.js
@@ -0,0 +1,12 @@
+var age = 22;
+var myname = "madhu";
+var isMarried = false;
+console.log("age = 22, type:",typeof age);
+console.log("myname = madhu, type:",typeof myName);
+console.log("isMarried = false, type:",typeof isMarried);
+
+
+var age = null;
+console.log(typeof age);
+ var city;
+ console.log(typeof city);
