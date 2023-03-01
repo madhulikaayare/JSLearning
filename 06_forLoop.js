c191a8983ef25601d49d63b513ea4efeb53f3685
madhulikaayare <madhulikaayare@gmail.com>
2023-03-01 07:58:20 +0530

    infinite loop

--- 06_forLoop.js
+++ 06_forLoop.js
@@ -13,3 +13,14 @@ for (let index = 10; index >= 0; index=index-1) {
    console.log(index);
     
 }
+//table of 5
+console.log("table of 5");
+for (let index = 5; index <= 50; index=index+5) {
+    console.log(index);
+    
+}
+console.log("********");
+for (let index = 1; index<=10;) {
+    console.log(index);
+    
+}
