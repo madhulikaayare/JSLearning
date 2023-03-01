88728d3e1ff87dca8bd6a20c8869a603d3dafc0b
madhulikaayare <madhulikaayare@gmail.com>
2023-03-01 07:42:47 +0530

    for loop

---
+++ 06_forLoop.js
@@ -0,0 +1,15 @@
+for (let index = 1; index <= 10; index = index+1) {
+   console.log(index);
+    
+}
+//print 0 to 5
+for (let index = 0; index <= 5; index= index+1) {
+    console.log(index);;
+    
+}
+//10 - 0
+console.log("reverse");
+for (let index = 10; index >= 0; index=index-1) {
+   console.log(index);
+    
+}
