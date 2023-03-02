344b2ad8a9f1a9bb790340da05ca2501225a253d
madhulikaayare <madhulikaayare@gmail.com>
2023-03-02 07:50:17 +0530

    updated

--- 04_string.js
+++ 04_string.js
@@ -1,71 +1,25 @@
-var greet = "Good Morning";
-console.log(typeof greet);
- 
-console.log("Total number of characters available into this string variable - greet");
-var greetlength = greet.length;
-console.log(greetlength);
+ var greet = "Good Morning";
+ console.log(typeof greet);
 
-console.log("Find character by index value");
-var charAtIndex3 = greet.charAt(3);
-console.log("character available at index 3 is:",charAtIndex3);
+ console.log("Total number of chars available into this string variable - greet");
+var greetLength = greet.length;
+ console.log(greetLength);
 
-console.log("Find the last character");
-var charAtLastIndex = greet.charAt(greetlength-1);
-console.log("last character is:", charAtLastIndex);
+ console.log("find the character at index");
+ var charAtIndex3 = greet.charAt(3);
+ console.log(charAtIndex3);
 
-console.log("find the index by character value ");
-var IndexOfM = greet.indexOf("M");
-console.log("Index of character M is :", IndexOfM);
+ console.log("find the last character");
+ var lastChar = greet.charAt(greetLength-1);
+ console.log(lastChar);
 
-console.log("index of char which is not available in string" ,greet.indexOf("Z") );
-console.log("Index of O char:",greet.indexOf("O"));
+ console.log("find index by char value ");
+ var indexOfM = greet.indexOf("M");
+ console.log(indexOfM);
 
-console.log("Index of o char:",greet.lastIndexOf("o"));
+ console.log("find index of char which is not available");
+ var indexOfZ = greet.indexOf("Z");
+ console.log(indexOfZ);
 
-var replaceResult  = greet.replace("Morning","Evening");
-console.log(replaceResult);
-console.log(greet);
-
-console.log("uppercase", replaceResult.toUpperCase());
-console.log("lowercase", replaceResult.toUpperCase());
-
-
-var myName = "   Mohit   Sharma  ";
-var myNameAfterTrim = myName.trim();
-console.log("Removing extra start and end spaces using trim(): ",myNameAfterTrim, myName.length, myNameAfterTrim.length);
-
-
-//find the total extra spaces removed from string - myName
-console.log("finding extra spaces removed:");
-var count = myName.length - myNameAfterTrim.length ;
-console.log(count);
-
-console.log(myName.trimStart(), myName.trimEnd());
-console.log("includes()");
-console.log("is substrin includes or not:", greet.includes("Mor"));
-console.log("is substrin includes or not:", greet.includes("Afternoon"));
-console.log("Is substring o includes in the greet or not: ", greet.includes("o"));
-
-console.log("slice() method");
-var sliceResult = greet.slice(5 , 11);
-console.log(sliceResult);
-console.log(greet.slice(2, 5));
-
-
-
-console.log("split() method");
-var greet = "Good Morning";
-var splitResult = greet.split(" ");
-console.log(splitResult);
-
-
-console.log("split() method");
-var greet = "Good Morning my dear friend";
-var splitResult = greet.split(" ");
-console.log(splitResult);
-console.log("Total words: ", splitResult.length);
-
-var myFriendList = "Billgates, Stew Job, Elon Musk, Ratan Tata, Sundar Pichai, Satya Nadella, Narayan Murti, Nandan Nilekani, Larry Page, Surgey Brain, Tim Cook";
-var myFriendList = myFriendList.split(" ");
-console.log(myFriendList);
-console.log(myFriendList.length);
+ console.log("index of o",greet.indexOf("o"));
+ console.log("index of o",greet.lastIndexOf("o"));
---
+++ 05_reverse_string.js
@@ -0,0 +1,23 @@
+var reverseString = function(str){
+    var reverse = ""; // wo
+    for (let index = str.length-1 ; index >= 0; index--) { // 9 
+       var charAt = str.charAt(index);
+       reverse = reverse + charAt; // "w"+"o" ==> "wo"
+    }
+    return reverse;
+}
+var result = reverseString("Do it anyhow"); 
+console.log(`Reverse String is: ${result}`);
+var reverseStr = function(str){ // str = "Do it anyhow"
+    var reverse = ""; // wohyna
+    for (let index = str.length-1 ; index >= 0; index--) {
+       var char = str.charAt(index);// o
+       if(char == " "){
+            break;
+       }
+       reverse = reverse + char; // "w"+"o" ==> "wo"
+    }
+    return reverse;
+}
+var result = reverseStr("Do it anyhow"); 
+console.log(`Reverse last word : ${result}`);
