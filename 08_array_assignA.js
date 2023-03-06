afce4b6b79e735f00797b85fd9181023b8482ede
madhulikaayare <madhulikaayare@gmail.com>
2023-03-06 15:21:08 +0530

    updated
    updated

--- 08_array_assignA.js
+++ 08_array_assignA.js
@@ -1,28 +1,36 @@
 const arraySeasonalFruits = ["Banana","Orange","Apple","Mango","Water Melon"];
+console.log("part 1");
 console.log("Banana" ,"Water Melon");
 
 var arrayFruits = ["Banana","Orange","Apple","Mango","Water Melon"];
 arrayFruits.unshift("Papaya");
+console.log("part 2");
 console.log(arrayFruits);
 
 var arrayFruits = ["Banana","Orange","Apple","Mango","Water Melon"];
 arrayFruits.splice(3,1);
+console.log("part 3");
 console.log(arrayFruits);
 
 var arrayFruits = ["Banana","Orange","Apple","Mango","Water Melon"];
 arrayFruits.push("Pineapple");
+console.log("part 4");
 console.log(arrayFruits);
 
 var arrayFruits = ["Banana","Orange","Apple","Mango","Water Melon"];
 arrayFruits.splice(4, 0, "Dragon Fruit")
+console.log("part 5");
 console.log(arrayFruits);
 
 var arrayFruits = ["Banana","Orange","Apple","Mango","Water Melon"];
 arrayFruits.splice(1,1,"Kiwi")
+console.log("part 6");
 console.log(arrayFruits);
 
 var arrayFruits = ["Orange","Apple","Mango","Water Melon"];
+console.log("part 7");
 console.log(arrayFruits);
 
 var arrayFruits = ["Apple","Mango","Water Melon"];
+console.log("part 8");
 console.log(arrayFruits);
