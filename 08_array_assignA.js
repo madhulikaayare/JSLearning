07b804272097dd299817c491213767a282f1a646
madhulikaayare <madhulikaayare@gmail.com>
2023-03-06 15:18:43 +0530

    array assingment

---
+++ 08_array_assignA.js
@@ -0,0 +1,28 @@
+const arraySeasonalFruits = ["Banana","Orange","Apple","Mango","Water Melon"];
+console.log("Banana" ,"Water Melon");
+
+var arrayFruits = ["Banana","Orange","Apple","Mango","Water Melon"];
+arrayFruits.unshift("Papaya");
+console.log(arrayFruits);
+
+var arrayFruits = ["Banana","Orange","Apple","Mango","Water Melon"];
+arrayFruits.splice(3,1);
+console.log(arrayFruits);
+
+var arrayFruits = ["Banana","Orange","Apple","Mango","Water Melon"];
+arrayFruits.push("Pineapple");
+console.log(arrayFruits);
+
+var arrayFruits = ["Banana","Orange","Apple","Mango","Water Melon"];
+arrayFruits.splice(4, 0, "Dragon Fruit")
+console.log(arrayFruits);
+
+var arrayFruits = ["Banana","Orange","Apple","Mango","Water Melon"];
+arrayFruits.splice(1,1,"Kiwi")
+console.log(arrayFruits);
+
+var arrayFruits = ["Orange","Apple","Mango","Water Melon"];
+console.log(arrayFruits);
+
+var arrayFruits = ["Apple","Mango","Water Melon"];
+console.log(arrayFruits);
