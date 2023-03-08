912a1ed3f4a43ff429db80dc28c8d6b671e6d510
madhulikaayare <madhulikaayare@gmail.com>
2023-03-08 08:34:48 +0530

    object

---
+++ 09_object.js
@@ -0,0 +1,13 @@
+let student =  {
+    firstName : "Madhulika",
+    lastName : "Ayare",
+    isWorking : false,
+    age : 22,
+    collegeName : "RMDSSOE",
+    id : 1234
+
+};
+console.log(student);
+console.table(student);
+console.log(typeof student);
+console.log(student.firstName);
