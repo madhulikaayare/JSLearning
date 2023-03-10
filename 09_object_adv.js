2b79609863b57ec622e5b6609180a675d467f793
madhulikaayare <madhulikaayare@gmail.com>
2023-03-10 07:05:14 +0530

    updated

--- 09_object_adv.js
+++ 09_object_adv.js
@@ -6,3 +6,28 @@ const employee = {
  }
  let resultAddress = student.addressDetails();
 console.log(resultAddress);
+
+for (const key in object) {
+    if (Object.hasOwnProperty.call(object, key)) {
+        const element = object[key];
+        
+    }
+}
+for (const key in employee) {
+    if (Object.hasOwnProperty.call(employee, key)) {
+        const element = employee[key];
+        console.log(key, element);
+    }
+}
+const keysOfEmployee = Object.keys(employee);
+console.log(keysOfEmployee);
+
+const valuesOfEmployee = Object.values(employee);
+console.log(valuesOfEmployee);
+
+const entriesOfEmployee = Object.entries(employee);
+console.log(entriesOfEmployee);
+
+console.log("==== in operator =====");
+ const isIdAvailable = "id" in employee;
+ console.log(isIdAvailable);
