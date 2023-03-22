4d161c4e288d69799bf9c8b03b2ae14d2f199125
madhulikaayare <madhulikaayare@gmail.com>
2023-03-22 08:54:50 +0530

    updated

--- 16_mapMethodAssigA.js
+++ 16_mapMethodAssigA.js
@@ -1,16 +1,16 @@
 const array = [20,11,40,25,23,11,9,31,60,2,19];
-
+console.log("========= add 10 to each element ============");
 const arrayTransformed = [];
 array.forEach( (currentValue) => {
     arrayTransformed.push(currentValue+10)
 } );
 console.log(arrayTransformed);
 
-
-console.log("Using map()");
+console.log("======= squaring ========");
 const arrayNew = array.map( (element) => {
    return element **2;
 } )
 console.log(arrayNew);
-
+ 
+console.log(" ====== index value addition ======");
 console.log([20,12,42,28,27,16,15,38,68,11,29]);
