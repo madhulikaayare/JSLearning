7a20c853d0a74798fcc8ead08ac150b8c2cf1386
madhulikaayare <madhulikaayare@gmail.com>
2023-03-23 07:42:27 +0530

    updated

--- 18_reduceMethod.js
+++ 18_reduceMethod.js
@@ -25,3 +25,9 @@ const sum = arrayNum.filter( (element)=> {
     return runningTotal + value;
 } );
 console.log(sum);
+
+// Find the numbers which are multiple of 3 from the given array and sum it
+console.log(" Find the numbers which are multiple of 3 from the given array and sum it");
+const sumMultipleOf3 = arrayNum.filter( element => element%3==0)
+.reduce( (runningTotal, value) => runningTotal + value);
+console.log(sum);
